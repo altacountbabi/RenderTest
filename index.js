66a3b1c7504b7b3fe30f9d2f31f7b10d7a9516d1
Whoman <82091823+altacountbabi@users.noreply.github.com>
2023-05-17 11:45:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const fetch = require('node-fetch');
 
 let options = {
-  method: 'GET',
-  headers: {
-    'Authorization': `Bearer ${process.env.apik}`
-  }
-};
+  	method: 'GET',
+  	headers: {
+    	'Authorization': `Bearer ${process.env.apik}`
+  	}
+}
 
 fetch('https://api.pawan.krd/info', options)
-  .then(response => response.json())
-  .then(json => {
-    delete json.info.name;
-    delete json.info.created;
-    console.log(json);
-  })
-  .catch(err => console.log(err));
-}
+  	.then(response => response.json())
+  	.then(json => {
+    	delete json.info.name
+    	delete json.info.created
+    	console.log(json)
+  	}).catch(err => console.log(err))
