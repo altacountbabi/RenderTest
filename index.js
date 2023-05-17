d785865b139abd5e5ee428ce453e0927626c8a9b
Whoman <82091823+altacountbabi@users.noreply.github.com>
2023-05-17 11:41:51 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+
+let options = {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${process.env.apik}`
+  }
+};
+
+fetch('https://api.pawan.krd/info', options)
+  .then(response => response.json())
+  .then(json => {
+    delete json.info.name;
+    delete json.info.created;
+    console.log(json);
+  })
+  .catch(err => console.log(err));
+}
