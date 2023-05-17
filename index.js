5cfe85fc6a26d2f2129358a774af544a00293ba4
Whoman <82091823+altacountbabi@users.noreply.github.com>
2023-05-17 11:46:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ fetch('https://api.pawan.krd/info', options)
   	.then(response => response.json())
   	.then(json => {
         delete json.info.name
-    	  delete json.info.created
-    	  console.log(json)
+        delete json.info.created
+    	console.log(json)
   	}).catch(err => console.log(err))
